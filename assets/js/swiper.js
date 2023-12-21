1ba3d4a7c7036cd531c8e49650405a606f6caedb
DoTanTu <95263083+DoTanTu@users.noreply.github.com>
2023-12-22 02:15:20 +0700

    change component

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,18 +1,25 @@
-var swiper = new Swiper(".news_banner", {
-    
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+var swiperNews = new Swiper(".news_banner", {
+  loop: true,
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+      delay: 5000, 
+      disableOnInteraction: false,
+  },
 });
 
-var swiper = new Swiper(".activities_banner", {
-    slidesPerView: 3,
-    spaceBetween: 2,
-    freeMode: true,
-    pagination: {
+var swiperActivities = new Swiper(".activities_banner", {
+  slidesPerView: 3,
+  spaceBetween: 2,
+  freeMode: true,
+  pagination: {
       el: ".swiper-pagination",
       clickable: true,
-    },
-  });
+  },
+  autoplay: {
+      delay: 3000, 
+      disableOnInteraction: false, 
+  },
+});
